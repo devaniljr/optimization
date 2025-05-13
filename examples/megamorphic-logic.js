4a495c891bebbe6e1ecf594465c39a89abf25d55
Devanil <contatodevanil@gmail.com>
2025-05-13 14:02:45 -0400

    fix: filter node internal lines

--- examples/megamorphic-logic.js
+++ examples/megamorphic-logic.js
@@ -2,15 +2,15 @@ function printX(obj) {
   console.log(obj.x);
 }
 
-// Monomorphic
-printX({ x: 1 });
-printX({ x: 2 });
-printX({ x: 3 });
+for (let i = 0; i < 10000; i++) {
+  printX({ x: i });
+}
 
-// Megamorphic
-printX({ x: 4, y: 0 }); 
-printX({ x: 5, z: true });
-printX({ x: 6, q: "hi" });
-printX(Object.create({ x: 7 }));
-printX([8]);
-printX({ get x() { return 9; }});
+printX({ x: 1, a: 1 });
+printX({ x: 1, b: 1 });
+printX({ x: 1, c: 1 });
+printX({ x: 1, d: 1 });
+printX({ x: 1, e: 1 });
+printX({ x: 1, f: 1 });
+printX({ x: 1, g: 1 });
+printX({ x: 1, h: 1 });
